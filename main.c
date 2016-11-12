a7dffc632135a34cd261d3211fb7327999867278
PARK <pwb910130@gmail.com>
2016-11-12 13:08:15 +0900

    문제 6 (난이도 : 中)*다음 식을 만족하는 자연수 a,b,c 의 개수를 구하여라i) a + b + c = 2000ii)a > b>c,a,b,c 는 모두 자연수

--- main.c
+++ main.c
@@ -4,17 +4,26 @@
 
 int main() {
     
-    int n=0;
-    int mul=1;
+    int a,b,c;
+    int count=0;
     
-    scanf("%d",&n);
-    
-    for(int i=1; i<n; i++){
-        mul = (mul*i)+mul;
+    for(a=2; a<2000; a++){
+        for(b=1; b<2000; b++){
+            
+            if(b>=a)
+                break;
+            for(c=0; c<2000; c++){
+                if(c>=b)
+                    break;
+                
+                if(a+b+c==2000)
+                    printf("%d, %d, %d\n",a,b,c);
+                count=count+1;
+            }
+        }
     }
+
+    printf("%d\n",count);
     
-    printf("%d",mul);
-
-
     return 0;
 }
