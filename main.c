aefd9531c7e348d558322e3042210d1f3dbbce5e
PARK <pwb910130@gmail.com>
2016-11-12 13:14:25 +0900

    도서 관리 프로그램을 만들어봅시다. 프로그램에는 다음과 같은 기능들이 구현되어 있어야 합니다. (난이도 : 上)1. 책을 새로 추가하는 기능 (책의 총 개수는 100 권이라 하자. 이 때, 각 책의 정보는 제목, 2. 저자의 이름, 출판사로 한다) 3. 책의 제목을 검색하면 그 책의 정보가 나와야 한다.4. 위와 마찬가지로 저자, 출판사 검색 기능이 있어야 한다. 5. 책을 빌리는 기능6. 책을 반납하는 기능

--- main.c
+++ main.c
@@ -1,58 +1,214 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int compare(char str1[], char str2[]);
-int a(char *s1, char *s2);
+
+//////  scanf("%[^\n]", str); -> \n 즉 엔터를 제외한 모든 문자열을 받는다.
+//////  fgets(<#char *restrict#>, sizeof(), stdin)
+
+void add_book(char (*book)[30],char (*author)[30],char (*publisher)[30],int *index);
+int search_book(char (*book)[30],char (*author)[30],char (*publisher)[30],int *borrow);
+int compare(char (*book)[],char *user_input);
+int borrow_book(char (*book)[],int *borrow);
+int return_book(char (*book)[],int *borrow);
+
 int main(){
     
     int flag;
-    char str1[100];
-    char str2[100];
+    char book_name[100][30];
+    char author[100][30];
+    char publisher[100][30];
+    int borrow[100]={0};
+    int index=0;
     
     while(1){
-        scanf("%s",str1);
-        scanf("%s",str2);
+        printf("1 : 책 추가\n");
+        printf("2 : 책 찾기\n");
+        printf("3 : 책 빌리기\n");
+        printf("4 : 책 반납하기\n");
+        printf("5 : 끝내기\n");
         
-        flag=compare(str1,str2);
+        scanf("%d",&flag);
         
-        if(flag==-1)
-            printf("There is no same word, %d\n",flag);
-        else
-            printf("%d\n",flag);
+        switch (flag) {
+            case 1:
+                add_book(book_name,author,publisher,&index);
+                index++;
+                break;
+            case 2:
+                search_book(book_name, author, publisher,borrow);
+                break;
+            case 3:
+                borrow_book(book_name,borrow);
+                break;
+            case 4:
+                return_book(book_name, borrow);
+                break;
+            case 5:
+                return 0;
+            default:
+                break;
+        }
+        
+        printf("\n");
+        printf("\n");
     }
+    
     return 0;
 }
 
-int compare(char str1[], char str2[]){
+void add_book(char (*book)[30],char (*author)[30],char (*publisher)[30],int *index){
     
-    int countstr2=0;
-    int count=1;
+    printf("*** 책 등록 ***\n");
+    printf("제목 : ");
+    scanf("%s",(book)[*index]);
+    printf("작가 : ");
+    scanf("%s",(author)[*index]);
+    printf("출판사 : ");
+    scanf("%s",(publisher)[*index]);
+    
+    printf("book name : %s, author name : %s, publisher name : %s, index : %d\n",(book)[*index],(book)[*index],(book)[*index],*index);
+}
+int search_book(char (*book)[30],char (*author)[30],char (*publisher)[30],int *borrow){
+    
+    char user_input[30];
+    
+    int flag;
+    printf("*** 책 찾기 ***\n");
+    printf("1 : 제목으로 찾기\n");
+    printf("2 : 작가 이름으로 책 찾기\n");
+    printf("3 : 출판사명으로 책 찾기\n");
+    printf("4 : 메뉴\n");
+    
+    scanf("%d",&flag);
+    
+    switch(flag){
+        case 1:
+            printf("제목 입력 : ");
+            scanf("%s",user_input);
+            for(int i=0; i<100; i++){
+                if(book[i][0]==user_input[0]){
+                    if(compare(book+(i*30),user_input)){
+                        if(borrow[i]==0)
+                            printf("제목 : %s, 저자 : %s, 출판사 : %s, 대여가능\n",&book[i][0],&author[i][0],&publisher[i][0]);
+                        else
+                            printf("제목 : %s, 저자 : %s, 출판사 : %s, 대여 불 가능\n",&book[i][0],&author[i][0],&publisher[i][0]);
+                        return 0;
+                    }
+                }
+                
+            }
+            printf("찾는 책이 없습니다.\n");
+            return 0;
+            break;
+            
+        case 2:
+            printf("저자 입력 : ");
+            scanf("%s",user_input);
+            for(int i=0; i<100; i++){
+                if(author[i][0]==user_input[0]){
+                    if(compare(author+(i*30),user_input)){
+                        if(borrow[i]==0)
+                            printf("제목 : %s, 저자 : %s, 출판사 : %s, 대여가능\n",&book[i][0],&author[i][0],&publisher[i][0]);
+                        else
+                            printf("제목 : %s, 저자 : %s, 출판사 : %s, 대여 불 가능\n",&book[i][0],&author[i][0],&publisher[i][0]);
+                        return 0;
+                    }
+                }
+                
+            }
+            printf("찾는 책이 없습니다.\n");
+            return 0;
+            break;
+            
+        case 3:
+            printf("출판사 입력 : ");
+            scanf("%s",user_input);
+            for(int i=0; i<100; i++){
+                if(publisher[i][0]==user_input[0]){
+                    if(compare(publisher+(i*30),user_input)){
+                        if(borrow[i]==0)
+                            printf("제목 : %s, 저자 : %s, 출판사 : %s, 대여가능\n",&book[i][0],&author[i][0],&publisher[i][0]);
+                        else
+                            printf("제목 : %s, 저자 : %s, 출판사 : %s, 대여 불 가능\n",&book[i][0],&author[i][0],&publisher[i][0]);
+                        return 0;
+                    }
+                }
+                
+            }
+            printf("찾는 책이 없습니다.\n");
+            return 0;
+            break;
+        case 4:
+            return 0;
+            break;
+    }
+    return 0;
+}
+int compare(char (*book)[],char *user_input){
+    
+    int i=0;
+    while((*book)[i]){
+        if(*user_input != (*book)[i])
+            return 0;
+        user_input++;
+        i++;
+    }
+    
+    if((*book)[i]==0)
+        return 1;
+    
+    return 0;
+}
+int borrow_book(char (*book)[30],int *borrow){
+    
+    char user_input[30];
+    
+    printf("대여할 책의 제목을 입력하세요 : ");
+    scanf("%s",user_input);
     
     for(int i=0; i<100; i++){
-        if(str1[i]=='_'){
-            count++;
+        if(book[i][0]==user_input[0]){
+            if(compare(book+(i*30),user_input)){
+                if(borrow[i]==0){
+                    borrow[i]=1;
+                    printf("%s를(을) 대여했습니다.",&book[i][0]);
+                    return 0;
+                }
+                else{
+                    printf("%s를(을) 대여할 수 없습니다.",&book[i][0]);
+                    return 0;
+                }
+            }
         }
         
-        if(str1[i]==str2[countstr2]){
-            if(a(&str1[i],&str2[countstr2]))
-                return count;
-        }
     }
-    
-    return -1;
+    printf("찾는 책이 없습니다.\n");
+    return 0;
 }
-
-int a(char *s1, char *s2){
+int return_book(char (*book)[30],int *borrow){
     
-    while(*s2){
-        if(*s2 != *s1)
-            return 0;
-        s1++;
-        s2++;
+    char user_input[30];
+    
+    printf("반납할 책의 제목을 입력하세요 : ");
+    scanf("%s",user_input);
+    
+    for(int i=0; i<100; i++){
+        if(book[i][0]==user_input[0]){
+            if(compare(book+(i*30),user_input)){
+                if(borrow[i]==1){
+                    borrow[i]=0;
+                    printf("%s를(을) 반납했습니다.",&book[i][0]);
+                    return 0;
+                }
+                else{
+                    printf("%s는(은) 이미 반납되었습니다.",&book[i][0]);
+                    return 0;
+                }
+            }
+        }
+        
     }
-    
-    if(*s1=='_'|| *s1=='\0')
-        return 1;
+    printf("찾는 책이 없습니다.\n");
     
     return 0;
 }
