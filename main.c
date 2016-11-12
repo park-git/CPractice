b6edf96c6fad966b1dfc3ba87b0b7cd25b763379
PARK <pwb910130@gmail.com>
2016-11-12 13:05:02 +0900

    문제 5(난이도 : 下)사용자로 부터 N 값을 입력 받고 1 부터 N 까지의 곱을 출력한다.

--- main.c
+++ main.c
@@ -4,24 +4,16 @@
 
 int main() {
     
-    int n1=1;
-    int n2=1;
-    int n3;
+    int n=0;
+    int mul=1;
     
-    int sum=1;
+    scanf("%d",&n);
     
-    for(int i=3; i<=1000000; i++){
-        
-        n3 = n1 + n2;
-        
-        n1 = n2;
-        n2 = n3;
-        
-        if(i%2==0)
-            sum = n3 + sum;
+    for(int i=1; i<n; i++){
+        mul = (mul*i)+mul;
     }
     
-    printf("%d\n",sum);
+    printf("%d",mul);
 
 
     return 0;
