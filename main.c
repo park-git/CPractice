7e04fa1bf2ac06b7c21a7a6212b3ef91fa8710b2
PARK <pwb910130@gmail.com>
2016-11-12 13:10:27 +0900

    생각해볼 문제 1) 사용자로 부터 5 명의 학생의 수학, 국어, 영어 점수를 입력 받아서 평균이 가장 높은 사람 부터 평균이 가장 낮은 사람까지 정렬되어 출력하도록 하세요. 특히, 평균을 기준으로 평균 이상인 사람 옆에는 '합격', 아닌 사람은 '불합격' 을 출력하게 해보세요.

--- main.c
+++ main.c
@@ -2,31 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+
+double bubble_sort(double *arr, int num_elements);
+
+double swap(double *pele);
+
+int main(){
     
-    int n;
+    double math[5] =    {5,4,2,3,1};
+    double korean[5] =  {5,4,2,3,1};
+    double eng[5] =     {5,4,2,3,1};
     
-    int K,start=2;
-    while(1)
-    {
-        scanf("%d",&n);
-        K=n;
-        start=2;
-        while(start<=K)
-        {
-            if(!(K%start))
-            {
-                printf("%d ",start);
-                K/=start;
+    double avr[5];
+    
+    double *pavr = NULL;
+    
+    for(int i = 0; i<5; i++ ){
+        avr[i] = (math[i] + korean[i] + eng[i]);
+        avr[i] = (avr[i]/3);
+    }
+    
+    pavr = avr;
+    
+    bubble_sort(pavr, 5);
+    
+    for(int i=0; i<5; i++)
+        printf("%f\n", avr[i]);
+    
+    return 0;
+}
+
+double bubble_sort(double *arr, int num_elements){
+    
+    int i,j;
+    
+    for(i=0; i<num_elements; i++){
+        for(j=0; j<num_elements-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr+j);
             }
-            else
-                start++;
-            
         }
-        
+        for(int k =0 ; k<5; k++){
+            printf("%f\n",arr[k]);
+        }
         printf("\n");
     }
+    
+    return 0;
+}
 
+double swap(double *pele){
+    
+    double tmp;
+    
+    tmp = *pele;
+    *pele = *(pele+1);
+    *(pele+1) = tmp;
     
     return 0;
 }
