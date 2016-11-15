579dcbed40aad9cb08edea424e5c737ae57c40ab
PARK <pwb910130@gmail.com>
2016-11-15 10:44:37 +0900

    파일 분할 연습 add header and c file

---
+++ str.c
@@ -0,0 +1,9 @@
+//
+//  str.c
+//  CPracitce
+//
+//  Created by PARK on 2016. 11. 15..
+//  Copyright © 2016년 PARK. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ str.h
@@ -0,0 +1,13 @@
+//
+//  str.h
+//  CPracitce
+//
+//  Created by PARK on 2016. 11. 15..
+//  Copyright © 2016년 PARK. All rights reserved.
+//
+
+#ifndef str_h
+#define str_h
+
+
+#endif /* str_h */
