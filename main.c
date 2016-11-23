1675e757bc1fe9cf2cc24da3e4030357e8825af5
PARK <pwb910130@gmail.com>
2016-11-23 16:11:50 +0900

    문제 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
    입력
    알파벳 소문자, 대문자 숫자 0-9 중 하나가 첫째 줄에 주어진다.
    
    출력
    입력으로 주어진 글자의 아스키 코드 값을 출력한다.

--- main.c
+++ main.c
@@ -5,39 +5,15 @@
 //////  scanf("%[^\n]", str); -> \n 즉 엔터를 제외한 모든 문자열을 받는다.
 //////  fgets(<#char *restrict#>, sizeof(), stdin)
 
-struct man {
-    int age;
-    int a[10];
-    char gender[10];
-    int *p;
-};
-
-void set_human(struct man *man,int age, char *gender){
-    
-    man->age = age;
-    strcpy(man->gender,gender);
-    
-}
-void print_info(struct man *man){
-    
-    printf("%d , %s\n",man->age,man->gender);
-    
-}
 int main(){
     
-    struct man man[3];
-    char gender[10];
-    int age;
+    char n;
     
-    for(int i=0; i<3; i++){
-        scanf("%d",&age);
-        scanf("%s",gender);
-        set_human(&man+i,age,gender);
-    }
+    scanf("%c",&n);
     
-    for(int i=0; i<3; i++){
-        print_info(&man+i);
-    }
+    printf("%d",n);
     
     return 0;
+
+    
 }
