d103cf7f7cb32ad2b16873ae6b363ee0a502256a
PARK <pwb910130@gmail.com>
2016-11-25 15:28:31 +0900

    무한히 큰 배열에 다음과 같이 분수들을 적혀있다.
    1/1	1/2	1/3	1/4	1/5	…
    2/1	2/2	2/3	2/4	…	…
    3/1	3/2	3/3	…	…	…
    4/1	4/2	…	…	…	…
    5/1	…	…	…	…	…
    …	…	…	…	…	…
    이와 같이 나열된 분수들을 1/1 -> 1/2 -> 2/1 -> 3/1 -> 2/2 -> … 과 같은 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
    
    X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 X(1≤X≤10,000,000)가 주어진다.
    
    출력
    첫째 줄에 분수를 출력한다.
    
    예제 입력  복사
    14
    예제 출력  복사
    2/4

--- main.c
+++ main.c
@@ -5,23 +5,28 @@
 //////  scanf("%[^\n]", str); -> \n 즉 엔터를 제외한 모든 문자열을 받는다.
 //////  fgets(<#char *restrict#>, sizeof(), stdin)
 
+int x,n=1,i=1;
+
 int main(){
     
-    int n,k=1,i=6,count=1;
-    
-    scanf("%d",&n);
+    scanf("%d",&x);
     
     while(1){
-    
-        if(n<=k)
+        if(x<=n)
             break;
         
-        k = k + i;
-        i = i+6;
-        count ++;
+        i++;
+        n = (i*(i+1))/2;
     }
     
-    printf("%d",count);
+    int k = n-x;
+    
+    if(i%2!=0){
+        printf("%d/%d",k+1,i-k);
+    }
+    else{
+        printf("%d/%d",i-k,k+1);
+    }
     
     return 0;
 }
