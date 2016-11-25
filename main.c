247e09b81b3cf8365cdfa41ebf1de3b83d3a5def
PARK <pwb910130@gmail.com>
2016-11-25 19:21:10 +0900

    스택 문제 정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
    명령은 총 다섯 가지이다.
    
    push X: 정수 X를 스택에 넣는 연산이다.
    pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
    size: 스택에 들어있는 정수의 개수를 출력한다.
    empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
    top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
    입력
    첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 둘쨰 줄부터 N개의 줄에는 명령이 하나씩 주어진다. 주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다. 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
    
    출력
    출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
    
    예제 입력  복사
    14
    push 1
    push 2
    top
    size
    empty
    pop
    pop
    pop
    size
    empty
    pop
    push 3
    empty
    top
    예제 출력  복사
    2
    2
    0
    2
    1
    -1
    0
    1
    -1
    0
    3

--- main.c
+++ main.c
@@ -5,32 +5,90 @@
 //////  scanf("%[^\n]", str); -> \n 즉 엔터를 제외한 모든 문자열을 받는다.
 //////  fgets(<#char *restrict#>, sizeof(), stdin)
 
+int arr[100000];
+int indexArr=-1;
 
-////////////////////// 소수 판별 함수 //////////////////////
-int isPrime(int num) {
-    
-    int i = 0;
-    
-    if (num < 2)
-        return 0;
-    for (i = 2; i*i <= num; i += 1) {
-        if (num % i == 0)
-            return 0;
-    }
-    return 1;
+void push(int x){
+    indexArr++;
+    arr[indexArr]=x;
 }
-////////////////////// 소수 판별 함수 //////////////////////
+int pop(){
+    if(indexArr==-1){
+        printf("-1\n");
+        return 0;
+    }
+    
+    printf("%d\n",arr[indexArr]);
+    arr[indexArr]='\0';
+    indexArr--;
+    
+    return 0;
+}
+int size(){
+    
+    printf("%d\n",indexArr+1);
+    
+    return 0;
+}
+int empty(){
+    
+    if(indexArr==-1){
+        printf("1\n");
+        return 0;
+    }
+    else{
+        printf("0\n");
+        return 0;
+    }
+}
+int top(){
+    
+    if(indexArr==-1){
+        printf("-1\n");
+        return 0;
+    }
+    
+    printf("%d\n",arr[indexArr]);
+    
+    return 0;
+}
 
 
 int main(){
     
-    int n,m;
+    int n,x;
+    char functionNmae[10];
+    scanf("%d",&n);
     
-    scanf("%d %d",&n,&m);
-    
-    for(int i=n; i<=m; i++)
-        if(isPrime(i))
-            printf("%d\n",i);
+    for(int i=0; i<n; i++){
+        scanf("%s",functionNmae);
+        
+        if(strcmp(functionNmae, "push")==0)
+        {
+            scanf("%d",&x);
+            push(x);
+        }
+        
+        else if(strcmp(functionNmae, "pop")==0)
+        {
+            pop();
+        }
+        
+        else if(strcmp(functionNmae, "size")==0)
+        {
+            size();
+        }
+        
+        else if(strcmp(functionNmae, "empty")==0)
+        {
+            empty();
+        }
+        
+        else if(strcmp(functionNmae, "top")==0)
+        {
+            top();
+        }
+    }
     
     return 0;
 }
