3cf7481205726b960b978d6efc4d0d8a950e1983
PARK <pwb910130@gmail.com>
2016-11-26 13:03:55 +0900

    DFS와 BFS 실패 문제집 시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율 5 초	128 MB	12135	3716	2194	28.175% 문제 그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다.
    입력
    첫째 줄에 정점의 개수 N(1≤N≤1,000), 간선의 개수 M(1≤M≤10,000), 탐색을 시작할 정점의 번호 V가 주어진다. 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. 한 간선이 여러 번 주어질 수도 있는데, 간선이 하나만 있는 것으로 생각하면 된다. 입력으로 주어지는 간선은 양방향이다.
    
    출력
    첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. V부터 방문된 점을 순서대로 출력하면 된다.
    
    예제 입력  복사
    4 5 1
    1 2
    1 3
    1 4
    2 4
    3 4
    예제 출력  복사
    1 2 4 3
    1 2 3 4

--- main.c
+++ main.c
@@ -4,148 +4,61 @@
 
 //////  scanf("%[^\n]", str); -> \n 즉 엔터를 제외한 모든 문자열을 받는다.
 //////  fgets(<#char *restrict#>, sizeof(), stdin)
-int push(int *arr,int *Front, int *Back, int x){
-    
-    if(arr[*Front] == 0)
-    {
-        arr[(*Front)] = x;
-        return 0;
-    }
-    
-    *Back = *Back+1;
-    arr[*Back]=x;
+int arr[1001][1001];
+int check[1001]={0};
+int queue[1001];
+int n,m,v,x,y,f=0,r=0;
 
-    return 0;
-}
-int pop(int *arr,int *Front, int *Back){
-    
-    if(*Front == *Back)
-    {
-        if(arr[*Front]!=0)
-        {
-            printf("%d\n",arr[*Front]);
-            arr[*Front]='\0';
-            return 0;
+void dfs(int v){
+    check[v] = 1;
+    printf("%d ",v);
+    for(int i=1; i<=n; i++){
+        if(arr[v][i]==1 && !check[i]){
+            dfs(i);
         }
-        
-        printf("-1\n");
-        return 0;
     }
-    
-    printf("%d\n",arr[*Front]);
-    
-    arr[*Front]='\0';
-    *Front=*Front+1;
-    
-    return 0;
+
 }
-int size(int *arr,int *Front, int *Back){
-    if(*Front == *Back)
-    {
-        if(arr[*Front]!=0)
-        {
-            printf("1\n");
-            return 0;
+void
+bfs(int v){
+    check[v]=1;
+    queue[r++]=v;
+    
+    while(f<r){
+        v = queue[f++];
+        printf("%d ",v);
+        for(int i=1; i<=n; i++){
+            if(arr[v][i]==1 &&  !check[i]){
+                check[i] = 1;
+                queue[r++]=i;
+            }
         }
-        
-        printf("0\n");
-        return 0;
     }
-    printf("%d\n",*Back-*Front+1);
-    return 0;
 }
-int empty(int *arr, int *Front,int *Back){
-    
-    if(*Front == *Back)
-    {
-        if(arr[*Front]!=0)
-        {
-            printf("0\n");
-            return 0;
-        }
-        
-        printf("1\n");
-        return 0;
-    }
-        printf("0\n");
-        return 0;
-}
-int front(int *arr,int *Front, int *Back){
-    if(*Front == *Back)
-    {
-        if(arr[*Front]!=0)
-        {
-            printf("%d\n",arr[*Front]);
-            return 0;
-        }
-        
-        printf("-1\n");
-        return 0;
-    }
-    
-    printf("%d\n",arr[*Front]);
-    
-    return 0;
-}
-int back(int *arr,int *Front, int *Back){
-    if(*Front == *Back)
-    {
-        if(arr[*Back]!=0)
-        {
-            printf("%d\n",arr[*Back]);
-            return 0;
-        }
-        
-        printf("-1\n");
-        return 0;
-    }
-    
-    printf("%d\n",arr[*Back]);
-    return 0;
-}
+
 int main(){
     
-    int n,x;
-    int arr[10000];
-    char msg[10];
-    int arrFront=0,arrBack=0;
     
-    scanf("%d",&n);
     
-    for(int i=0; i<n; i++){
-        scanf("%s",msg);
-        
-        switch (msg[0]) {
-            case 'p':
-                
-                if(msg[1]=='u'){
-                    scanf("%d",&x);
-                    push(arr,&arrFront,&arrBack,x);
-                }
-                else
-                    pop(arr,&arrFront,&arrBack);
-                break;
-                
-            case 's':
-                size(arr,&arrFront,&arrBack);
-                break;
-                
-            case 'e':
-                empty(arr,&arrFront,&arrBack);
-                break;
-                
-            case 'f':
-                front(arr,&arrFront,&arrBack);
-                break;
-                
-            case 'b':
-                back(arr,&arrFront,&arrBack);
-                break;
-                
-            default:
-                break;
+    scanf("%d %d %d",&n,&m,&v);
+    
+    for(int i=0; i<m; i++){
+        scanf("%d %d",&x,&y);
+        arr[x][y] = arr[y][x]=1;
+    }
+    
+    for(int i=1; i<m-1; i++){
+        for(int j=1; j<m-1; j++){
+            
         }
     }
+    
+    dfs(v);
+    printf("\n");
+    for(int i=0; i<1000; i++)
+        check[i] = 0;
+    
+    bfs(v);
     
     return 0;
 }
